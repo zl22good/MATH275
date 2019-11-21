87f0c73289ec6bb272a3110274531563445b0f2f
zl22good <zl22good@siena.edu>
2019-11-21 00:53:05 -0500

    final project

--- FinalProject_GoodsellZ.R
+++ FinalProject_GoodsellZ.R
@@ -1,3 +1,24 @@
+library(dplyr)
+library(stringr)
+library(readr)
+library(lubridate)
+
+
+firedata <- read.csv("PropertyLoss.csv")
+#head(firedata)
+firedata$Incident.Num <- strtoi(substr(firedata$Inci_type,0,3))
+#head(firedata)
+firedata$Incident.Type <- transform(firedata, c= ifelse(Incident.Num < "124", "Structure Fire", ifelse(Incident.Num < "139", "Vehicle Fire",ifelse(Incident.Num < "165", "Outside Fire", "Not a Fire"))))
+#head(firedata)
+firedata$Month<- toString(firedata$Alm_Date)
+head(firedata)
+
+
+for (i in length(firedata$Month)) {
+  firedata$Month[i] = "a"
+}
+
+
 firedata <- read.csv("PropertyLossEdited.csv")
 head(firedata)
 #Season
@@ -5,9 +26,9 @@ head(firedata[3])
 #Type
 head(firedata[6])
 
-libary(dplry)
+library(dplyr)
 
-anova(firedata)
+
 
 #Anova test of total loss based on season
 aovSeason = aov(Total_Loss ~ Season, data = firedata)
@@ -17,3 +38,50 @@ summary(aovSeason)
 aovI = aov(Total_Loss ~ Incident.Type, data = firedata)
 summary(aovI)
 
+#Total loss summary of each season
+firedata %>%
+  group_by(Season) %>%
+  summarize(Md = median(Total_Loss), Mean = mean(Total_Loss), SD = sd(Total_Loss), N = n())
+
+#Winter Subset
+winter <- firedata %>%
+  filter(Season == "Winter") %>%
+  group_by(Season) %>%
+  select(Season, Incident.Type,Total_Loss)
+
+#Spring Subset
+spring <- firedata %>%
+  filter(Season == "Spring") %>%
+  group_by(Season) %>%
+  select(Season, Incident.Type,Total_Loss)
+
+#Summer Subset
+summer <- firedata %>%
+  filter(Season == "Summer") %>%
+  group_by(Season) %>%
+  select(Season, Incident.Type,Total_Loss)
+
+#Fall Subset
+fall <- firedata %>%
+  filter(Season == "Autumn") %>%
+  group_by(Season) %>%
+  select(Season, Incident.Type,Total_Loss)
+
+#Box plot of all the subsets
+boxplot(winter$Total_Loss,spring$Total_Loss,summer$Total_Loss,fall$Total_Loss,
+        names = c("Winter","Spring","Summer","Fall"), xlab = "Season", ylab = "Total Loss")
+
+#T test, winter and spring 
+t.test(winter$Total_Loss,spring$Total_Loss)
+
+#T test, winter and summer 
+t.test(winter$Total_Loss,summer$Total_Loss)
+
+#T test, winter and fall 
+t.test(winter$Total_Loss,fall$Total_Loss)
+
+
+#Total loss summary of each season based on incident type
+firedata %>%
+  group_by(Season, Incident.Type) %>%
+  summarize(Md = median(Total_Loss), Mean = mean(Total_Loss), SD = sd(Total_Loss), N = n())
